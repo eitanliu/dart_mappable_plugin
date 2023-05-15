1c8acd79cfe54a6c92750234703b6beb177e6ba2
Eitan Liu <eitanliu@gmail.com>
2023-05-16 01:52:06 +0800

    add graph extension

--- src/main/kotlin/com/eitanliu/dart_mappable/extensions/PropertyGraphExt.kt
+++ src/main/kotlin/com/eitanliu/dart_mappable/extensions/PropertyGraphExt.kt
@@ -6,6 +6,12 @@ import com.intellij.openapi.observable.properties.GraphProperty
 import com.intellij.openapi.observable.properties.GraphPropertyImpl
 import com.intellij.openapi.observable.properties.PropertyGraph
 import kotlin.reflect.KMutableProperty0
+import kotlin.reflect.KProperty0
+
+@Suppress("NOTHING_TO_INLINE", "UnstableApiUsage")
+inline fun <V> KProperty0<V>.toGraphProperty(
+    propertyGraph: PropertyGraph = PropertyGraph()
+): GraphProperty<V> = GraphPropertyImpl(propertyGraph, ::get)
 
 @Suppress("NOTHING_TO_INLINE", "UnstableApiUsage")
 inline fun <V> KMutableProperty0<V>.toGraphProperty(
@@ -15,9 +21,18 @@ inline fun <V> KMutableProperty0<V>.toGraphProperty(
         if (get() != it) set(it)
     }
 }
+@Suppress("NOTHING_TO_INLINE")
+inline fun <T> PropertyGraph.propertyOf(ref: KProperty0<T>): GraphProperty<T> = ref.toGraphProperty(this)
 
-@Suppress("NOTHING_TO_INLINE", "EXTENSION_SHADOWED_BY_MEMBER")
+@Suppress("NOTHING_TO_INLINE")
 inline fun <T> PropertyGraph.propertyOf(ref: KMutableProperty0<T>): GraphProperty<T> = ref.toGraphProperty(this)
+
+@Suppress("NOTHING_TO_INLINE", "UnstableApiUsage")
+inline fun <T> PropertyGraph.propertyValue(initial: T): GraphProperty<T> = GraphPropertyImpl(this) { initial }
+
+@Suppress("NOTHING_TO_INLINE", "UnstableApiUsage")
+inline fun <T> PropertyGraph.propertyValue(noinline initial: () -> T): GraphProperty<T> =
+    GraphPropertyImpl(this, initial)
 
 inline var <T> GraphProperty<T>.value
     get() = get()

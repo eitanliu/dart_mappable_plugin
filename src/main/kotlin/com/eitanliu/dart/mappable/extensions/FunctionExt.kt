e6dbe66a07a967ad44895e6b142e92dbb78d64d1
Eitan Liu <eitanliu@gmail.com>
2023-05-22 03:29:53 +0800

    merge patch 231 versions

---
+++ src/main/kotlin/com/eitanliu/dart/mappable/extensions/FunctionExt.kt
@@ -0,0 +1,48 @@
+package com.eitanliu.dart.mappable.extensions
+
+import com.intellij.util.containers.WeakList
+import java.lang.ref.WeakReference
+
+fun <T> (T.() -> Unit).weakRef(): T.() -> Unit {
+    return WeakFun1Reference(this)
+}
+
+class WeakFun1Reference<T>(
+    func: T.() -> Unit
+) : Function1<T, Unit> {
+
+    private val func = WeakReference(func)
+
+    override fun invoke(p1: T) {
+        func.get()?.invoke(p1)
+    }
+
+}
+
+class WeakRecFun1List<T>(
+    receiver: T, list: WeakList<T.() -> Unit> = WeakList()
+) : Function0<Unit>, MutableCollection<T.() -> Unit> by list {
+
+    private val weakRef = WeakReference(this)
+
+    private val receiver: WeakReference<T>
+
+    val listener = Listener(weakRef)
+
+    init {
+        this.receiver = WeakReference(receiver)
+    }
+
+    override fun invoke() = forEach {
+        receiver.get()?.apply { it.invoke(this) }
+    }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    class Listener<T>(val weakRef: WeakReference<WeakRecFun1List<T>>) : Function0<Unit> {
+        override fun invoke() {
+            weakRef.get()?.invoke()
+        }
+
+    }
+
+}

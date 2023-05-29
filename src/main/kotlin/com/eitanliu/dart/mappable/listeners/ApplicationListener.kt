046848f25c496b19bd8f9331a50983655cbcb095
Eitan Liu <eitanliu@gmail.com>
2023-05-30 02:58:07 +0800

    update project config

---
+++ src/main/kotlin/com/eitanliu/dart/mappable/listeners/ApplicationListener.kt
@@ -0,0 +1,20 @@
+package com.eitanliu.dart.mappable.listeners
+
+import com.eitanliu.dart.mappable.DartMappableBundle
+import com.intellij.ide.AppLifecycleListener
+
+
+/**
+ *     <applicationListeners>
+ *         <listener class="com.eitanliu.dart.mappable.listeners.ApplicationListener"
+ *             topic="com.intellij.ide.AppLifecycleListener" />
+ *     </applicationListeners>
+ *
+ * import com.intellij.ide.AppLifecycleListener
+ */
+internal class ApplicationListener : AppLifecycleListener {
+
+    override fun appFrameCreated(commandLineArgs: List<String>) {
+        println(DartMappableBundle.message("application.listener"))
+    }
+}
---
+++ src/main/kotlin/com/eitanliu/dart/mappable/listeners/ProjectListener.kt
@@ -0,0 +1,37 @@
+package com.eitanliu.dart.mappable.listeners
+
+import com.eitanliu.dart.mappable.services.ProjectService
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManagerListener
+
+
+/**
+ *     <applicationListeners>
+ *         <listener class="com.eitanliu.dart.mappable.listeners.ProjectListener"
+ *             topic="com.intellij.openapi.project.ProjectManagerListener" />
+ *     </applicationListeners>
+ *
+ * import com.intellij.openapi.project.ProjectManagerListener
+ */
+internal class ProjectListener : ProjectManagerListener {
+
+    override fun projectOpened(project: Project) {
+        project.service<ProjectService>()
+    }
+}
+
+/**
+ *     <extensions defaultExtensionNs="com.intellij">
+ *         <postStartupActivity implementation="com.eitanliu.dart.mappable.listeners.ProjectListener" />
+ *     </extensions>
+ *
+ * before 231
+ * import com.intellij.openapi.startup.StartupActivity
+ */
+// internal class ProjectListener : StartupActivity {
+//     override fun runActivity(project: Project) {
+//         project.service<ProjectService>()
+//
+//     }
+// }

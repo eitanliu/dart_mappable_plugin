1426d314024888e54e9fe297437c548d83f0ff4b
Eitan Liu <eitanliu@gmail.com>
2023-07-18 21:57:28 +0800

    add freezed generator

--- src/test/kotlin/com/eitanliu/dart/mappable/GeneratorUnitTest.kt
+++ src/test/kotlin/com/eitanliu/dart/mappable/GeneratorUnitTest.kt
@@ -2,14 +2,12 @@ package com.eitanliu.dart.mappable
 
 import com.eitanliu.dart.mappable.ast.CodeGenerator
 import com.eitanliu.dart.mappable.entity.PubspecEntity
-import com.eitanliu.dart.mappable.generator.DartMappableGenerator
-import com.eitanliu.dart.mappable.generator.JsonSerializableGenerator
+import com.eitanliu.dart.mappable.generator.buildDartGenerator
 import com.eitanliu.dart.mappable.settings.Implements
 import com.eitanliu.dart.mappable.settings.Settings
 import com.eitanliu.dart.mappable.utils.DependenciesUtils
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -65,6 +63,7 @@ class GeneratorUnitTest {
         val content = jsonParse()
 
         val setting = Settings().apply {
+            implement = Implements.DART_MAPPABLE
             modelSuffix = "Vo"
             constructor = false
             nullable = true
@@ -73,7 +72,7 @@ class GeneratorUnitTest {
             enableJsonReflectable = false
             enableMixin = true
         }
-        val generator = DartMappableGenerator(setting, "Text01", content)
+        val generator = buildDartGenerator(setting, "Text01", content)
         val classes = generator.buildString()
         println("classes: \n$classes")
     }
@@ -83,12 +82,29 @@ class GeneratorUnitTest {
         val content = jsonParse()
 
         val setting = Settings().apply {
+            implement = Implements.JSON_SERIALIZABLE
             modelSuffix = "Vo"
             constructor = false
             nullable = true
             final = false
         }
-        val generator = JsonSerializableGenerator(setting, "Text01", content)
+        val generator = buildDartGenerator(setting, "Text01", content)
+        val classes = generator.buildString()
+        println("classes: \n$classes")
+    }
+
+    @Test
+    fun testFreezed() {
+        val content = jsonParse()
+
+        val setting = Settings().apply {
+            implement = Implements.FREEZED
+            modelSuffix = "Vo"
+            constructor = false
+            nullable = false
+            final = true
+        }
+        val generator = buildDartGenerator(setting, "Text01", content)
         val classes = generator.buildString()
         println("classes: \n$classes")
     }
